04a1d263dca43fa2d331562b6f36c7216d2dde3e
Qbius <theqbius@gmail.com>
2019-12-04 10:18:33 +0100

    nicer formatting

--- day4.erl
+++ day4.erl
@@ -4,7 +4,14 @@
 fst() ->
     {ok, Data} = file:read_file("day4.input"),
     [LowerBound, UpperBound] = lists:map(fun list_to_integer/1, string:split(string:trim(binary_to_list(Data)), "-", all)),
-    AllPass = [list_to_integer([A, B, C, D, E, F]) || A <- lists:seq($1, $9), B <- lists:seq(A, $9), C <- lists:seq(B, $9), D <- lists:seq(C, $9), E <- lists:seq(D, $9), F <- lists:seq(E, $9), A =:= B orelse B =:= C orelse C =:= D orelse D =:= E orelse E =:= F],
+    AllPass = [
+        list_to_integer([A, B, C, D, E, F]) || A <- lists:seq($1, $9), B <- lists:seq(A, $9), C <- lists:seq(B, $9), D <- lists:seq(C, $9), E <- lists:seq(D, $9), F <- lists:seq(E, $9), 
+        A =:= B orelse 
+        B =:= C orelse 
+        C =:= D orelse 
+        D =:= E orelse 
+        E =:= F
+    ],
     length(lists:filter(fun(N) -> N >= LowerBound andalso N =< UpperBound end, AllPass)).
 
 snd() ->
