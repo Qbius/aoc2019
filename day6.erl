b2457a0e8ab111bf60ab55e95781cea4ef13c458
Qbius <theqbius@gmail.com>
2019-12-06 11:56:02 +0100

    part2 + slightly unifying the code

--- day6.erl
+++ day6.erl
@@ -1,19 +1,30 @@
 -module(day6).
 -export([fst/0, snd/0]).
 
-get_orbits(Object, OrbitMap) ->
-    case OrbitMap of
-        #{Object := OrbitedObject} -> 1 + get_orbits(OrbitedObject, OrbitMap);
-        _ -> 0
-    end.
-
 fst() ->
     {ok, Data} = file:read_file("day6.input"),
     OrbitMap = maps:from_list([list_to_tuple(lists:reverse(string:split(Line, ")"))) || Line <- string:split(binary_to_list(Data), "\r\n", all)]),
     AllObjects = sets:to_list(sets:union(sets:from_list(maps:keys(OrbitMap)), sets:from_list(maps:values(OrbitMap)))),
     lists:foldl(fun(Object, Acc) ->
-        Acc + get_orbits(Object, OrbitMap)
+        Acc + length(get_orbits(Object, OrbitMap))
     end, 0, AllObjects).
 
 snd() ->
-    ok.
+    {ok, Data} = file:read_file("day6.input"),
+    OrbitMap = maps:from_list([list_to_tuple(lists:reverse(string:split(Line, ")"))) || Line <- string:split(binary_to_list(Data), "\r\n", all)]),
+    My_orbits = get_orbits("YOU", OrbitMap),
+    Sn_orbits = get_orbits("SAN", OrbitMap),
+    UpToCommonNode = fun(List) -> lists:takewhile(fun(E) -> E =/= first_common(My_orbits, Sn_orbits) end, List) end,
+    length(UpToCommonNode(My_orbits)) + length(UpToCommonNode(Sn_orbits)).
+
+get_orbits(Object, OrbitMap) ->
+    case OrbitMap of
+        #{Object := OrbitedObject} -> [OrbitedObject | get_orbits(OrbitedObject, OrbitMap)];
+        _ -> []
+    end.
+
+first_common([H|T], OtherList) ->
+    case lists:member(H, OtherList) of
+        true -> H;
+        false -> first_common(T, OtherList)
+    end.
