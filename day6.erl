fbdea3554233a1992885067443656abc5600de75
Qbius <theqbius@gmail.com>
2019-12-06 12:04:45 +0100

    nicer formatting

--- day6.erl
+++ day6.erl
@@ -2,20 +2,21 @@
 -export([fst/0, snd/0]).
 
 fst() ->
-    {ok, Data} = file:read_file("day6.input"),
-    OrbitMap = maps:from_list([list_to_tuple(lists:reverse(string:split(Line, ")"))) || Line <- string:split(binary_to_list(Data), "\r\n", all)]),
-    AllObjects = sets:to_list(sets:union(sets:from_list(maps:keys(OrbitMap)), sets:from_list(maps:values(OrbitMap)))),
+    OrbitMap = get_orbit_map(),
     lists:foldl(fun(Object, Acc) ->
         Acc + length(get_orbits(Object, OrbitMap))
-    end, 0, AllObjects).
+    end, 0, maps:keys(OrbitMap)).
 
 snd() ->
-    {ok, Data} = file:read_file("day6.input"),
-    OrbitMap = maps:from_list([list_to_tuple(lists:reverse(string:split(Line, ")"))) || Line <- string:split(binary_to_list(Data), "\r\n", all)]),
+    OrbitMap = get_orbit_map(),
     My_orbits = get_orbits("YOU", OrbitMap),
     Sn_orbits = get_orbits("SAN", OrbitMap),
     UpToCommonNode = fun(List) -> lists:takewhile(fun(E) -> E =/= first_common(My_orbits, Sn_orbits) end, List) end,
     length(UpToCommonNode(My_orbits)) + length(UpToCommonNode(Sn_orbits)).
+
+get_orbit_map() ->
+    {ok, Data} = file:read_file("day6.input"),
+    maps:from_list([list_to_tuple(lists:reverse(string:split(Line, ")"))) || Line <- string:split(binary_to_list(Data), "\r\n", all)]).
 
 get_orbits(Object, OrbitMap) ->
     case OrbitMap of
